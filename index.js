ae4d591014d974226e8f96981e7afeeef5dcabcb
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-03-22 20:26:55 -0300

    feat: implementada a função que retorna o end point /talker com o array do arquivo talker.json

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 
+const getSetTalker = require('./services');
+
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const talkers = await getSetTalker.getTalker();
+  return res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
