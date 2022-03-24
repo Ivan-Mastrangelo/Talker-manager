97eb0639aed18c61e4f4db3c909738469f97089b
Ivan Mastrangelo Dias <masttrangello@gmail.com>
2022-03-24 11:54:27 -0300

    refactor: limpando o arquivo

--- routes/talkerRoutes.js
+++ routes/talkerRoutes.js
@@ -2,55 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 const getSetTalker = require('../services/getSetData');
-const validToken = require('./middlewares/validToken');
-
-const nameAndAgeValid = (req, res, next) => {
-  const { name, age } = req.body;
-  if (!name) return res.status(400).json({ message: 'O campo "name" é obrigatório' });
-  if (name.length < 3) {
-    return res
-    .status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
-  }
-  if (!age) return res.status(400).json({ message: 'O campo "age" é obrigatório' });
-  if (age < 18) {
-    return res.status(400)
-    .json({ message: 'A pessoa palestrante deve ser maior de idade' });
-  }
-
-  next();
-};
-
-const validDate = (req, res, next) => {
-  const { talk } = req.body;
-  if (!talk) {
-  return res.status(400)
-  .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-} 
-  const { watchedAt } = talk;
-  const dataRegex = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/; // referência: https://stackoverflow.com/questions/15491894/regex-to-validate-date-formats-dd-mm-yyyy-dd-mm-yyyy-dd-mm-yyyy-dd-mmm-yyyy
-  const isWatchedAt = dataRegex.test(watchedAt);
-  if (!watchedAt) {
-    return res.status(400)
-    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-    } 
-  if (!isWatchedAt) {
-    return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
-  }
-  next();
-};
-
-const validRate = (req, res, next) => {
-  const { talk } = req.body;
-  const { rate } = talk;
-  if (rate === undefined) {
-    return res.status(400)
-    .json({ message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios' });
-  }
-  if (rate < 1 || rate > 5) {
-    return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
-  }
-  next();
-};
+const {
+  validToken,
+  nameAndAgeValid,
+  validDate,
+  validRate,
+} = require('../middlewares');
 
 router.get('/', async (req, res) => {
   const talkers = await getSetTalker.getTalker();
